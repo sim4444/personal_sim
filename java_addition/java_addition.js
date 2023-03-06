240adcceb992ae0dfd030bd9d76c9b399b3f10a1
Simran <samcanadian44@gmail.com>
2023-03-06 12:22:57 -0800

    created a constant array called courseList and assigned some items in it and I assigned 5 items with different names

---
+++ java_addition/java_addition.js
@@ -0,0 +1,24 @@
+const courseList = [c0,c1,c2,c4,c5];
+let c0 = {code:"ACIT 1420", name :"Intro to System Administration"}
+let c1 = {code:"ACIT 1620", name :"Web Technologies"}
+let c2 = {code:"ACIT 1630", name :"Database"}
+let c4 = {code:"ACIT 1515", name :"Scripting"}
+let c5 = {code:"COMM 1116", name :"Business Communication"}
+
+
+
+
+// function my_courses(code,name){
+//     this.code=code;
+//     this.name=name;
+// }
+// const c0 = new my_courses("ACIT 1420","Intro to System Administration")
+// const c1 = new my_courses("ACIT 1620","Web Technologies")
+// const c2 = new my_courses("ACIT 1630","Database")
+// const c4 = new my_courses("ACIT 1515","Scripting")
+// const c5 = new my_courses("COMM 1116","Business Communication")
+// console.log(c0);
+// console.log(c0);
+// console.log(c0);
+// console.log(c0);
+// console.log(c0);
