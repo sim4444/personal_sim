0976f0faabb42bb501b555b4622742979ee294f6
Simran <samcanadian44@gmail.com>
2023-03-06 12:46:58 -0800

    added do while loop to practice the inputs from user and set a condition as mentioned in question

--- java_addition/java_addition.js
+++ java_addition/java_addition.js
@@ -4,13 +4,24 @@ let c1 = {code:"ACIT 1620", name :"Web Technologies"};
 let c2 = {code:"ACIT 1630", name :"Database"};
 let c4 = {code:"ACIT 1515", name :"Scripting"};
 let c5 = {code:"COMM 1116", name :"Business Communication"};
+// console.log(user_answer);
+// console.log(alert(user_answer));
 let user_answer = promt("Please enter a 4-digit number: ");
-if (user_answer (isNaN)){
+do{
+    user_answer = promt("Please enter a 4-digit number: ");
+}while(user_answer.length !== 4 && user_answer === (isNaN)){
     console.log(`This entry : ${user_answer} is invalid. Please enter again:`)
+
+};
+for (let item of courseList){
+    
 }
-else{
-    console.log(alert('Hello'))
-}
+
+
+
+// else{
+//     console.log(alert('Hello'))
+// }
 
 
 
