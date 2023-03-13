8ddf7ef9d99d4f0f5707315ac69c1aba73b709bf
Simran <samcanadian44@gmail.com>
2023-03-12 20:35:27 -0700

    testing my code

--- java_addition/java_addition.js
+++ java_addition/java_addition.js
@@ -1,9 +1,11 @@
-const courseList = [course1,course2,course3,course4,course5];
 let course1 = {code:"1420", name :"Intro to System Administration"};
 let course2 = {code:"1620", name :"Web Fundamental Technologies"};
 let course3 = {code:"1630", name :"Database"};
 let course4 = {code:"1515", name :"Scripting for IT"};
 let course5 = {code:"1310", name :"Technical Mathematics for IT"};
+
+const courseList = [course1,course2,course3,course4,course5];
+
 let user_answer = prompt("Please enter a 4-digit number: ");
 
 // console.log(user_answer);
@@ -16,10 +18,11 @@ let user_answer = prompt("Please enter a 4-digit number: ");
 //     }
 // }
 while(user_answer.length !== 4 || isNaN(user_answer)){
-    console.log(`This entry : ${user_answer} is invalid. Please enter again:`)
+    console.log(`${user_answer} is an invalid entry. Please enter again!!`);
+    user_answer = prompt("Please enter a 4-digit number again: ");
 };
 for (let item of courseList){
-    if (item.code===user_answer){
+    if (item.code.includes(user_answer)){
         console.log(`Yes I am taking the course: ACIT ${item.code} - ${item.name}`);
     }
     break
